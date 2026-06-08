85513729d519b8d748e97f97fd4299ced51000c0
zephygg <kamaiarnur@gmail.com>
2026-06-08 22:06:04 +0500

    L16: Person notifies the screen via INotifyPropertyChanged

--- SchoolApp/MainPage.xaml.cs
+++ SchoolApp/MainPage.xaml.cs
@@ -20,5 +20,10 @@ public partial class MainPage : ContentPage
     {
         await Shell.Current.GoToAsync(nameof(StudentsPage));
     }
+    private void OnShoutClicked(object sender, EventArgs e)
+    {
+        if (BindingContext is Person p)
+            p.Name = p.Name.ToUpper();
+    }
 }
 
---
+++ SchoolApp/ViewModels/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace SchoolApp.ViewModels;
+
+public class Person : INotifyPropertyChanged
+{
+    private string _name = "Aida";
+    public string Name
+    {
+        get => _name;
+        set
+        {
+            _name = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Name)));
+        }
+    }
+
+    private string _city = "Almaty";
+    public string City
+    {
+        get => _city;
+        set
+        {
+            _city = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(City)));
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+}
